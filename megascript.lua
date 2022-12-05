f0106e2afa76c4ef958b0f226e3177c8762ce070
Scrooll <119859019+Scrooll@users.noreply.github.com>
2022-12-05 18:29:33 +0500

    Update megascript.lua

--- megascript.lua
+++ megascript.lua
@@ -14,6 +14,27 @@ Section:NewSlider("Прыжок", "SliderInfo", 500, 0, function(s) -- 500 (Ма
       game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 end)
 
+Section:NewButton("вх", "Esp что то типо wallhack)", function()
+while wait(0.5) do
+    for i, childrik in ipairs(workspace:GetDescendants()) do
+        if childrik:FindFirstChild("Humanoid") then
+            if not childrik:FindFirstChild("EspBox") then
+                if childrik ~= game.Players.LocalPlayer.Character then
+                    local esp = Instance.new("BoxHandleAdornment",childrik)
+                    esp.Adornee = childrik
+                    esp.ZIndex = 0
+                    esp.Size = Vector3.new(4, 5, 1)
+                    esp.Transparency = 0.65
+                    esp.Color3 = Color3.fromRGB(255,48,48)
+                    esp.AlwaysOnTop = true
+                    esp.Name = "EspBox"
+                end
+            end
+        end
+    end
+end
+		
+
 Section:NewButton("Aimbot", "Аимбот для шутеров(Не рекомендуется включать в режимах где нет ничего с аимом)", function()
     PLAYER  = game.Players.LocalPlayer
 MOUSE   = PLAYER:GetMouse()
