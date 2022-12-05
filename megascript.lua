485dbae1799bac70904f5b9ec7560407c1beac2e
Scrooll <119859019+Scrooll@users.noreply.github.com>
2022-12-05 17:59:37 +0500

    Update megascript.lua

--- megascript.lua
+++ megascript.lua
@@ -281,8 +281,8 @@ local Tab = Window:NewTab("Mine")
 local Section = Tab:NewSection("Функции")
 
 Section:NewButton("Moneysss", "Просто тпает к вам монеты)", function()
-for i,v in pairs(game:GetDescendants()) do
-if v.Name == 'Credit' then
-v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+	for i,v in pairs(game:GetDescendants()) do
+	if v.Name == 'Credit' then
+	v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 end
 end
