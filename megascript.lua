2a9b0d9ae5d6bf1100fd625470f4c99efd0a9610
Scrooll <119859019+Scrooll@users.noreply.github.com>
2023-02-03 11:44:57 +0500

    Update megascript.lua

--- megascript.lua
+++ megascript.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Lodochka Hub", "RJTheme8")
+local Window = Library.CreateLib("Lodochka Hub", "RJTheme4")
 
 local Tab = Window:NewTab("Основные функции")
 
