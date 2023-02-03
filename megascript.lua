cb95b819990cdb0d8805bcdd2bcf902e58cad08a
Scrooll <119859019+Scrooll@users.noreply.github.com>
2023-02-03 17:27:43 +0500

    Update megascript.lua

--- megascript.lua
+++ megascript.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Widthblade", "Midnight")
 
 local Tab = Window:NewTab("Основные функции")
