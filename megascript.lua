ba80e5f9dfed018544533d9df7a464e34dce0055
Scrooll <119859019+Scrooll@users.noreply.github.com>
2023-04-04 08:07:59 +0500

    Update megascript.lua

--- megascript.lua
+++ megascript.lua
@@ -1,5 +1,5 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Widthblade", "Midnight")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "widthblade", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 local Tab = Window:NewTab("Основные функции")
 
