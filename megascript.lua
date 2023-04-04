92d958354cf2de2c511a898f2d06d441ed3444af
Scrooll <119859019+Scrooll@users.noreply.github.com>
2023-04-04 10:10:43 +0500

    Update megascript.lua

--- megascript.lua
+++ megascript.lua
@@ -1,7 +1,10 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "widthblade", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local Tab = Window:NewTab("Основные функции")
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
 
 local Section = Tab:NewSection("Самые основы")
 
